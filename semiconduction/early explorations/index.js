2f466638d1a212bf1e45f80efc225a97b36925ba
kathy wu <kaaathywu@gmail.com>
2021-08-08 16:12:43 -0400

    new computer, mysteries abound
    also made new poem

---
+++ semiconduction/early explorations/index.js
@@ -0,0 +1,10 @@
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// const element = <h1>hello world</h1>;
+
+
+//render ReactDOM
+// ReactDOM.render(element, document.getElementById('root'));
