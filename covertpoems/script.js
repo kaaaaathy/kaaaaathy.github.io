62f3a1e9642ce524d8735b56ad216eac74740d75
kathy wu <kaaathywu@gmail.com>
2021-08-17 13:15:38 -0400

    small changes to example

--- covertpoems/script.js
+++ covertpoems/script.js
@@ -1,4 +1,4 @@
-let desires = ["lick your flower bud","taste your mouth","enter, again", "suck a tenderness", "and graze you","caressing an edge"];
+let desires = ["lick your flower bud","taste your mouth","enter, again", "suck a tenderness", "and graze you","caressing an edge","enter, again","wanting"];
 
 let alongYou = window.innerHeight;
 let acrossYou = window.innerWidth;
@@ -8,11 +8,11 @@ for (let i = 0; i<desires.length;i++){
   let newDesire = document.createElement("p");
   newDesire.innerText = desires[i];
 
-// PUT IN A RANDOM LOCATION, adding 50px padding on all sides, 200px preventing it from getting too close to right side
-  newDesire.style.top = getRandomNumber(50, innerHeight-50);
+// PUT IN A RANDOM LOCATION, adding 50px padding and 200px padding to prevent it from getting too close to right side
+  newDesire.style.top = getRandomNumber(50, innerHeight-200);
   newDesire.style.left = getRandomNumber(50, innerWidth-200);
 
-// TELL IT TO CHANGE ITS APPEARANCE WHEN TOUCHED (CAN DELETE)
+// CHANGE TEXT APPEARANCE WHEN TOUCHED (CAN DELETE)
   newDesire.addEventListener("click", function(){
       newDesire.classList.toggle("touched");
   });
@@ -20,7 +20,7 @@ for (let i = 0; i<desires.length;i++){
   document.body.appendChild(newDesire);
 }
 
-// A FUNCTION THAT GIVES A RANDOM NUMBER, FOR SIMPLICITY
+// A FUNCTION THAT GIVES A RANDOM NUMBER (optional, I just like to simplify the code this way)
 function getRandomNumber(min,max){
   return Math.floor(Math.random() * max + min);
 }
