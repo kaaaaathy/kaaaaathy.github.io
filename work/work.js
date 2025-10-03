710014650d8d79d97d1f29bd01a9fb8a321e072c
kathy wu <kaaathywu@gmail.com>
2025-10-03 14:20:00 -0400

    add embedded vid

--- work/work.js
+++ work/work.js
@@ -38,6 +38,11 @@ This project was also an experiment in expanding audience telepresence online; I
 [broadcast6.png]
     The weather divination session ends when enough cursors congregate around the center of the sky, and create a fully white "eclipse" in the projection space.
 
+
+<br>
+WEBSITE
+You can still access the divination website here → <a href="https://www.uncertain.cloud/" target="_blank">LINK</a>
+ <iframe src="https://player.vimeo.com/video/1123723725" frameborder="0" width="100%" height ="320"></iframe>
 <br>
 METEOROLOGICAL GLYPHS
 A set of glyphs drawn from meteorological symbols, turned into a custom typeface used for balloons, and for looping ground film projected through haze. 
@@ -75,7 +80,8 @@ A set of glyphs drawn from meteorological symbols, turned into a custom typeface
   [still3.png]
   [still2.png]
 
-  <div style="padding:56.26% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/947107796#t=0m55s?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="she will last as long as stones (reading)"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+  <div style="padding:56.26% 0 0 0;position:relative;">
+  <iframe src="https://player.vimeo.com/video/947107796#t=0m55s?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="she will last as long as stones (reading)"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
     <br>
   GARMENT
   A cold, 15-pound garment stitched together with Rhode Island river and beach stones. This garment is worn during the computer performance as an expression of embodied grief, and as an extension of the local ecologies. 
