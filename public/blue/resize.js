2aea871667a89a78a5b29e91e40c96f908f6213c
kathy wu <kaaathywu@gmail.com>
2026-05-01 22:40:51 -0400

    Update index.css, made repos for each project

--- public/blue/resize.js
+++
@@ -1,7 +0,0 @@
-let cont = document.getElementById('container');
-cont.style.height = window.innerHeight + "px";
-
-window.addEventListener('resize',function(){
-    winWidth = window.innerWidth;
-    cont.style.height = window.innerHeight + "px";
-});
